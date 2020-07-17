a2bc0b5cf27176e3c676a9d3fd231b9049c72413
triplej <his1monologue@gmail.com>
2020-07-17 11:52:55 +0900

    사진들 다 지우기

--- src/routes/resultApp.js
+++ src/routes/resultApp.js
@@ -2,79 +2,78 @@ import React from 'react'
 import Result from '../components/result';
 
 
-import pic1 from '../image/보영/170713.jpg';
-import pic2 from '../image/보영/180723 보영소현.jpg';
-import pic3 from '../image/보영/180811.jpg';
-import pic4 from '../image/보영/180829.jpg';
-import pic5 from '../image/보영/181031.jpg';
-import pic6 from '../image/보영/181225.jpg';
-import pic7 from '../image/보영/190103.jpg';
-import pic8 from '../image/보영/190131.jpg';
+import pic1 from '../image/sample.jpg';
+import pic2 from '../image/sample.jpg';
+import pic3 from '../image/sample.jpg';
+import pic4 from '../image/sample.jpg';
+import pic5 from '../image/sample.jpg';
+import pic6 from '../image/sample.jpg';
+import pic7 from '../image/sample.jpg';
+import pic8 from '../image/sample.jpg';
 
-import pic9 from '../image/산하/130511첫촬.jpg';
-import pic10 from '../image/산하/160519.jpg';
-import pic11 from '../image/산하/160520.jpg';
-import pic12 from '../image/산하/180423.jpg';
-import pic13 from '../image/산하/180907.jpg';
-import pic14 from '../image/산하/190324 집들이.jpg';
-import pic15 from '../image/산하/190616.jpg';
-import pic16 from '../image/산하/190908.jpg';
-import pic17 from '../image/산하/191004.jpg';
-import pic18 from '../image/산하/200220.jpg';
+import pic9 from '../image/sample.jpg';
+import pic10 from '../image/sample.jpg';
+import pic11 from '../image/sample.jpg';
+import pic12 from '../image/sample.jpg';
+import pic13 from '../image/sample.jpg';
+import pic14 from '../image/sample.jpg';
+import pic15 from '../image/sample.jpg';
+import pic16 from '../image/sample.jpg';
+import pic17 from '../image/sample.jpg';
+import pic18 from '../image/sample.jpg';
+
+import pic19 from '../image/sample.jpg';
+import pic20 from '../image/sample.jpg';
+import pic21 from '../image/sample.jpg';
+import pic22 from '../image/sample.jpg';
+import pic23 from '../image/sample.jpg';
+import pic24 from '../image/sample.jpg';
+import pic25 from '../image/sample.jpg';
+import pic26 from '../image/sample.jpg';
+import pic27 from '../image/sample.jpg';
+import pic28 from '../image/sample.jpg';
+
+import pic31 from '../image/sample.jpg';
+import pic32 from '../image/sample.jpg';
+import pic33 from '../image/sample.jpg';
+import pic34 from '../image/sample.jpg';
+import pic35 from '../image/sample.jpg';
+import pic36 from '../image/sample.jpg';
+import pic37 from '../image/sample.jpg';
+import pic38 from '../image/sample.jpg';
+import pic39 from '../image/sample.jpg';
+import pic40 from '../image/sample.jpg';
+import pic41 from '../image/sample.jpg';
+import pic42 from '../image/sample.jpg';
+import pic43 from '../image/sample.jpg';
+import pic44 from '../image/sample.jpg';
 
 
-import pic19 from '../image/소현/130318.png';
-import pic20 from '../image/소현/130331.png';
-import pic21 from '../image/소현/130419.jpg';
-import pic22 from '../image/소현/130722.jpg';
-import pic23 from '../image/소현/160520.jpg';
-import pic24 from '../image/소현/180119.jpg';
-import pic25 from '../image/소현/190227.jpg';
-import pic26 from '../image/소현/190706.jpg';
-import pic27 from '../image/소현/190708.jpg';
-import pic28 from '../image/소현/191228.jpg';
+import pic45 from '../image/sample.jpg';
+import pic46 from '../image/sample.jpg';
+import pic47 from '../image/sample.jpg';
+import pic48 from '../image/sample.jpg';
+import pic49 from '../image/sample.jpg';
+import pic50 from '../image/sample.jpg';
+import pic51 from '../image/sample.jpg';
+import pic52 from '../image/sample.jpg';
 
-import pic31 from '../image/연희/120417.png';
-import pic32 from '../image/연희/131005.jpg';
-import pic33 from '../image/연희/131115.jpg';
-import pic34 from '../image/연희/150523.jpg';
-import pic35 from '../image/연희/150607.jpg';
-import pic36 from '../image/연희/150714.jpg';
-import pic37 from '../image/연희/150923.jpg';
-import pic38 from '../image/연희/151010.jpg';
-import pic39 from '../image/연희/160505.jpg';
-import pic40 from '../image/연희/161119.jpg';
-import pic41 from '../image/연희/170826.jpg';
-import pic42 from '../image/연희/181224.jpg';
-import pic43 from '../image/연희/190219.jpg';
-import pic44 from '../image/연희/190622.jpg';
-
-
-import pic45 from '../image/진솔/1302 (2).jpg';
-import pic46 from '../image/진솔/130413.jpg';
-import pic47 from '../image/진솔/160731.jpg';
-import pic48 from '../image/진솔/171015.jpg';
-import pic49 from '../image/진솔/190215.jpg';
-import pic50 from '../image/진솔/190629.jpg';
-import pic51 from '../image/진솔/190803.jpg';
-import pic52 from '../image/진솔/190918.jpg';
-
-import pic53 from '../image/한나/130207.jpg';
-import pic54 from '../image/한나/130208.jpg';
-import pic55 from '../image/한나/130417.png';
-import pic56 from '../image/한나/130827.jpg';
-import pic57 from '../image/한나/150712.jpg';
-import pic58 from '../image/한나/150824.jpg';
-import pic59 from '../image/한나/150923.jpg';
-import pic60 from '../image/한나/160227.jpg';
-import pic61 from '../image/한나/160316.jpg';
-import pic62 from '../image/한나/160515.jpg';
-import pic63 from '../image/한나/160619.jpg';
-import pic64 from '../image/한나/160905.jpg';
-import pic65 from '../image/한나/170825.jpg';
-import pic66 from '../image/한나/180824 수영장펜션.jpg';
-import pic67 from '../image/한나/190901.jpg';
-import pic68 from '../image/한나/191107.jpg';
+import pic53 from '../image/sample.jpg';
+import pic54 from '../image/sample.jpg';
+import pic55 from '../image/sample.jpg';
+import pic56 from '../image/sample.jpg';
+import pic57 from '../image/sample.jpg';
+import pic58 from '../image/sample.jpg';
+import pic59 from '../image/sample.jpg';
+import pic60 from '../image/sample.jpg';
+import pic61 from '../image/sample.jpg';
+import pic62 from '../image/sample.jpg';
+import pic63 from '../image/sample.jpg';
+import pic64 from '../image/sample.jpg';
+import pic65 from '../image/sample.jpg';
+import pic66 from '../image/sample.jpg';
+import pic67 from '../image/sample.jpg';
+import pic68 from '../image/sample.jpg';
 
 
 
